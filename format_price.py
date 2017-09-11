754059b9ad2297d4eade0115e7bf383f6c6ad5ed
Timur Jaganov <andeniel@gmail.com>
2017-09-11 17:18:45 +0600

    completed the assignment and sent it for review

--- format_price.py
+++ format_price.py
@@ -1,5 +1,42 @@
+import argparse
+import re
+
+
 def format_price(price):
-    pass
+    price_str = str(price).strip()
+    clear_str = re.sub(r"[., ]", "", price_str)
+    if not clear_str.isdigit():
+        return None
+
+    digit_str = price_str.split('.')
+
+    numbers_str = re.sub(
+        r"[, ]", "",
+        digit_str[0]) if len(digit_str) > 0 else ""
+    decimal_str = re.sub(
+        r"[., ]", "",
+        digit_str[1]) if len(digit_str) > 1 else ""
+
+    reverse_price = numbers_str[::-1]
+    space_price = ' '.join(
+        [reverse_price[key_index:key_index+3] for key_index in range(
+            0, len(reverse_price), 3)])
+    decimal_str = decimal_str.rstrip("0")
+
+    return "{}{}".format(
+            space_price[::-1],
+            '.'+decimal_str if len(decimal_str) > 0 else "")
 
 if __name__ == '__main__':
-    pass
+    aparser = argparse.ArgumentParser()
+    aparser.add_argument(
+        'price_input',
+        type=float,
+        help="Price input string to format"
+    )
+    args = aparser.parse_args()
+    price = format_price(args.price_input)
+    if price:
+        print(price)
+    else:
+        print("Введенное число не допустимое")
--- tests.py
+++ tests.py
@@ -1,1 +1,36 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_int_format(self):
+        price = format_price(2000)
+        self.assertEqual(price, "2 000")
+
+    def test_string_format(self):
+        price = format_price("2437")
+        self.assertEqual(price, "2 437")
+
+    def test_float_format(self):
+        price = format_price(3245.000)
+        self.assertEqual(price, "3 245")
+
+    def test_decimal_format(self):
+        price = format_price(3245.1250)
+        self.assertEqual(price, "3 245.125")
+
+    def test_string_float_format(self):
+        price = format_price("3245.23")
+        self.assertEqual(price, "3 245.23")
+
+    def test_none_number(self):
+        price = format_price('surname')
+        self.assertIsNone(price)
+
+    def test_mix_number(self):
+        price = format_price('24839 USD')
+        self.assertIsNone(price)
+
+
+if __name__ == '__main__':
+    unittest.main()
